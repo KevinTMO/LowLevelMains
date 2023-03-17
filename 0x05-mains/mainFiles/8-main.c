d274ee0357d85f484e9448882030f608dd5b7324
Kevin Ramos <kevin.ramos3@hotmail.com>
2023-03-17 23:37:44 +0000

    Adding last mainfile for task 8

---
+++ 0x05-mains/mainFiles/8-main.c
@@ -0,0 +1,29 @@
+#include "roadmap.h"
+
+/**
+*main - will call rot13() to encode a string using rot13
+*Return: will return 0 if success
+*/
+
+int main(void)
+{
+	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+	char *p;
+
+	p = rot13(s);
+	printf("%s", p);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	printf("------------------------------------\n");
+	p = rot13(s);
+	printf("%s", p);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	printf("------------------------------------\n");
+	p = rot13(s);
+	printf("%s", p);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	
+	return (0);
+}
